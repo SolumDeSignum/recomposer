346afe9204d96822e1566f8d5e37cc9d329fa407
Oskars Germovs <oskars_germovs@inbox.lv>
2024-10-18 19:48:08 +0300

    1.) added: DeferrableProvider 2.) added: standardization 3.) added: code format
    Signed-off-by: Oskars Germovs <oskars_germovs@inbox.lv>

--- src/ReComposerServiceProvider.php
+++ src/ReComposerServiceProvider.php
@@ -4,20 +4,11 @@ declare(strict_types=1);
 
 namespace SolumDeSignum\ReComposer;
 
+use Illuminate\Contracts\Support\DeferrableProvider;
 use Illuminate\Support\ServiceProvider;
 
-class ReComposerServiceProvider extends ServiceProvider
+class ReComposerServiceProvider extends ServiceProvider implements DeferrableProvider
 {
-    /**
-     * @var string
-     */
-    public static string $alias = 'recomposer';
-
-    /**
-     * @var string
-     */
-    public static string $namespaceSuffix = 'solumdesignum';
-
     /**
      * Boot up the package. Load the views from the correct directory.
      */
@@ -25,18 +16,12 @@ class ReComposerServiceProvider extends ServiceProvider
     {
         $this->loadViewsFrom(
             __DIR__ . '/../resources/views',
-            self::$namespaceSuffix . '/' . self::$alias
+            'solumdesignum/recomposer'
         );
 
+        // Publishing is only necessary when using the CLI.
         if ($this->app->runningInConsole()) {
-            $this->publishes(
-                [
-                    __DIR__ . '/../config/recomposer.php' => config_path(
-                        'recomposer.php'
-                    ),
-                ],
-                'config'
-            );
+            $this->bootForConsole();
         }
     }
 
@@ -45,6 +30,29 @@ class ReComposerServiceProvider extends ServiceProvider
         $this->mergeConfigFrom(
             __DIR__ . '/../config/recomposer.php',
             'recomposer'
+        );
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides(): array
+    {
+        return parent::provides();
+    }
+
+    protected function bootForConsole(): void
+    {
+        // Publishing the configuration file.
+        $this->publishes(
+            [
+                __DIR__ . '/../config/recomposer.php' => config_path(
+                    'recomposer.php'
+                ),
+            ],
+            'config'
         );
     }
 }
